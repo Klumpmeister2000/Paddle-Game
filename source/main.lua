384b80d09bfa02896863df95c5990ffe4f240aed
klumpmeister <christian@klumpmeister.net>
2023-09-07 10:16:08 -0400

    updated formatting and removed extra text

--- source/main.lua
+++ source/main.lua
@@ -17,8 +17,10 @@ function Ball:init()
   gfx.fillCircleAtPoint(radius, radius, radius)
   gfx.popContext()
   self:setImage(ballImage)
+  self:setCollideRect(0, 0, self:getSize())
   self:moveTo(200, 120)
   self.xSpeed = 5
+  
 end
 
 function Ball:update()
@@ -31,31 +33,17 @@ function Ball:update()
         end
 end
 
-function Ball:update()
-    -- returns actualX, actualY, a list of collisions, and the
-    -- length of the set of collisions
-    --
-    -- actualX and actualY represent where the sprite ended up
-    -- after the collisions were applied and it was moved outside
-    -- the bounds of any sprites it collided with. But for now
-    -- we only care if it needs to bounce or not. :)
-    --
-    -- We're only going to use the list of collisions right now,
-    -- so the convention in Lua is to use _ for unused variables
-    local _, _, collisions, _ = self:moveWithCollisions(self.x + self.xSpeed, self.y)
   
-    -- In Lua, #collection gives you the length of the object,
-    -- similar to collection.length in other languages
+  function Ball:update()
+    local _, _, collisions, _ = self:moveWithCollisions(self.x + self.xSpeed, self.y + self.ySpeed)
+  
     for i = 1, #collisions do
-      -- just for testing purposes
-      print(collisions[i].normal)
-      -- if the ball should bounce horizontally, then invert
-      -- its xSpeed
-      --
-      -- also ~= is "not equals" in Lua, similar to != in
-      -- most other languages 
       if collisions[i].normal.x ~= 0 then
         self.xSpeed *= -1
+      end
+  
+      if collisions[i].normal.y ~= 0 then
+        self.ySpeed *= -1
       end
     end
   end
@@ -75,8 +63,14 @@ ball:add()
 leftWall = gfx.sprite.addEmptyCollisionSprite(-5, 0, 5, 240)
 leftWall:add()
 
-rightWall= gfx.sprite.addEmptyCollisionSprite(400, 0, 5, 240)
+rightWall = gfx.sprite.addEmptyCollisionSprite(400, 0, 5, 240)
 rightWall:add()
+
+topWall = gfx.sprite.addEmptyCollisionSprite(0, -5, 400, 5)
+topWall:add()
+
+bottomWall = gfx.sprite.addEmptyCollisionSprite(0, 240, 400, 5)
+bottomWall:add()
 
 function playdate.update()
     gfx.sprite.update()
