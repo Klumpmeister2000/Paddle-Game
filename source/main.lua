2e3841749a085da82a2c16f519e9ab3e47314c4d
klumpmeister <christian@klumpmeister.net>
2023-09-07 13:14:59 -0400

    reverted changes to back to beginning makingball

--- source/main.lua
+++ source/main.lua
@@ -6,68 +6,22 @@ gfx = playdate.graphics
 
 class("Ball").extends(gfx.sprite)
 
-
 function Ball:init()
-
   Ball.super.init(self)
-  
+
   radius = 5
   local ballImage = gfx.image.new(2 * radius, 2 * radius)
   gfx.pushContext(ballImage)
   gfx.fillCircleAtPoint(radius, radius, radius)
   gfx.popContext()
   self:setImage(ballImage)
-  self:setCollideRect(0, 0, self:getSize())
+
   self:moveTo(200, 120)
-  self.xSpeed = 5
-  self.ySpeed = 6
-
-end
-
-function Ball:update()
-    local _, _, collisions, _ = self:moveWithCollisions(self.x + self.xSpeed, self.y + self.ySpeed)
-    if self.x + self.xSpeed >= 400 
-        then
-        self.xSpeed *= -1
-    elseif self.x + self.xSpeed <= 0 
-        then
-        self.xSpeed *= -1
-    end
-
-    for i = 1, #collisions do
-      if collisions[i].normal.x ~= 0 then
-        self.xSpeed *= -1
-      end
-  
-      if collisions[i].normal.y ~= 0 then
-        self.ySpeed *= -1
-end
-
-
-function Ball:init()
-    -- etc
-  
-    self:setImage(ballImage)
-    self:setCollideRect(0, 0, self:getSize())
-  
-    self:moveTo(200, 120)
 end
 
 ball = Ball()
 ball:add()
 
-leftWall = gfx.sprite.addEmptyCollisionSprite(-5, 0, 5, 240)
-leftWall:add()
-
-rightWall = gfx.sprite.addEmptyCollisionSprite(400, 0, 5, 240)
-rightWall:add()
-
-topWall = gfx.sprite.addEmptyCollisionSprite(0, -5, 400, 5)
-topWall:add()
-
-bottomWall = gfx.sprite.addEmptyCollisionSprite(0, 240, 400, 5)
-bottomWall:add()
-
 function playdate.update()
-    gfx.sprite.update()
+  gfx.sprite.update()
 end
